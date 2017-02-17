a36384aa1141c4bc7009b57aa37496a2a5c6f381
rogerbf <roger@bergfalk.nu>
2017-02-17 22:25:15 +0100

    streaming-top -> watch-pid

--- src/lib/watcher.js
+++ src/lib/watcher.js
@@ -1,32 +1,8 @@
-const pmset = process.argv[2]
-const surveyed = process.argv[3]
+const watch = require(`watch-pid`)
+const pmsetPid = process.argv[2]
+const subjectPid = process.argv[3]
 
-const isRunning = (processes, pid) => {
-  return processes.filter(proc => proc.PID === pid).length === 1
-}
-
-const top = require('streaming-top')({ delay: 3 })
-
-top.on('data', data => {
-  
-  const processes = JSON.parse(data.toString()).processes
-
-  if (isRunning(processes, pmset)) {
-    if (!isRunning(processes, surveyed)) {
-      process.kill(parseInt(pmset))
-      process.exit()
-    }
-  } else {
-    process.exit()
-  }
-
-})
-
-process.on('exit', () => {
-  top.kill()
-})
-
-process.on('uncaughtException', err => {
-  top.kill()
-  throw err
+watch(subjectPid, () => {
+  process.kill(pmsetPid)
+  process.exit()
 })
